28a488b862716141f49213fe0f986693984f765e
Gustavo210 <gustavogontijoo@gmail.com>
2025-06-23 10:41:09 -0300

    gaps

--- defaultSizes.ts
+++ defaultSizes.ts
@@ -1,21 +1,31 @@
-import tools from '@mobilestock-native/tools'
+import tools from "@mobilestock-native/tools";
 
 export default {
   layout: {
     space: {
-      xs: tools.calculateSpacing('0.75rem'),
-      sm: tools.calculateSpacing('0.875rem'),
-      md: tools.calculateSpacing('1rem'),
-      lg: tools.calculateSpacing('1.25rem'),
-      xl: tools.calculateSpacing('1.75rem'),
-      '2xl': tools.calculateSpacing('1.875rem'),
-      '3xl': tools.calculateSpacing('2rem')
+      xs: tools.calculateSpacing("0.75rem"),
+      sm: tools.calculateSpacing("0.875rem"),
+      md: tools.calculateSpacing("1rem"),
+      lg: tools.calculateSpacing("1.25rem"),
+      xl: tools.calculateSpacing("1.75rem"),
+      "2xl": tools.calculateSpacing("1.875rem"),
+      "3xl": tools.calculateSpacing("2rem"),
     },
     columns: {
       xs: 1,
       sm: 2,
       md: 3,
-      lg: 4
-    }
-  }
-}
+      lg: 4,
+    },
+    gaps: {
+      "2xs": "0.25rem",
+      xs: "0.5rem",
+      sm: "0.75rem",
+      md: "1rem",
+      lg: "1.25rem",
+      xl: "1.5rem",
+      "2xl": "1.75rem",
+      "3xl": "2rem",
+    },
+  },
+};
