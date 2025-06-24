c3de97ca6fbadedc483088bee23fc8b3991b175a
Gustavo210 <gustavogontijoo@gmail.com>
2025-06-24 16:05:35 -0300

    Refactor RootLayout to enhance header functionality and improve navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,14 @@ import "react-native-reanimated";
 
 import { GlobalStyle } from "@/globals";
 import ThemeProvider from "@/themeProvider";
-import { Platform, Text } from "react-native";
+import {
+  Button,
+  Platform,
+  Pressable,
+  SafeAreaView,
+  Text,
+  View,
+} from "react-native";
 
 export default function RootLayout() {
   const router = useRouter();
@@ -19,26 +26,75 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider>
-      <Stack
-        screenOptions={{
-          headerTitle: (props) => {
-            return (
-              <Text style={{ fontFamily: "SpaceMono", fontSize: 18 }}>
-                {Platform.OS === "web" ? "Web" : "Mobile"}
-              </Text>
-            );
-          },
-          title: "Expo Router",
-        }}
-      >
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-        <Stack.Screen name="minha-rota" />
-        <Stack.Screen name="teste/minha-rota" />
-      </Stack>
-      <StatusBar style="dark" />
-      {Platform.OS === "web" && <GlobalStyle />}
-    </ThemeProvider>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+      <ThemeProvider>
+        <Stack
+          screenOptions={{
+            headerTitle: (props) => {
+              return (
+                <Text style={{ fontFamily: "SpaceMono", fontSize: 18 }}>
+                  {Platform.OS === "web" ? "Web" : "Mobile"}
+                </Text>
+              );
+            },
+            headerRight: () => (
+              <Button title="Configs" onPress={() => alert("Opa")} />
+            ),
+            header: () => {
+              return (
+                <View
+                  style={{
+                    paddingTop: 30,
+                    height: 80,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    backgroundColor: "#f8f8f8",
+                  }}
+                >
+                  <Button title="Voltar" onPress={() => router.back()} />
+                  <Text
+                    style={{
+                      fontFamily: "SpaceMono",
+                      flex: 1,
+                      fontSize: 18,
+                      textAlign: "center",
+                    }}
+                  >
+                    {Platform.OS === "web" ? "Web" : "Mobile"}
+                  </Text>
+                  <Pressable
+                    style={{
+                      padding: 10,
+                      backgroundColor: "#e0e0e0",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                    onPress={() => router.push("/configs")}
+                  >
+                    <Text>Config</Text>
+                  </Pressable>
+                </View>
+              );
+            },
+            title: "Expo Router",
+          }}
+        >
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+          <Stack.Screen name="minha-rota" />
+          <Stack.Screen name="teste/minha-rota" />
+          <Stack.Screen
+            name="configs"
+            options={{
+              headerShown: false,
+              animation: "fade_from_bottom",
+              presentation: "transparentModal",
+            }}
+          />
+        </Stack>
+        <StatusBar style="dark" />
+        {Platform.OS === "web" && <GlobalStyle />}
+      </ThemeProvider>
+    </SafeAreaView>
   );
 }
