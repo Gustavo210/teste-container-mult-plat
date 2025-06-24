448448910a2496f2c4cc5cde5101f2fe1ef7638b
Gustavo210 <gustavogontijoo@gmail.com>
2025-06-24 17:10:48 -0300

    Refactor defaultSizes to use dynamic imports for tools and enhance font size calculations

--- defaultSizes.ts
+++ defaultSizes.ts
@@ -1,11 +1,21 @@
-import tools from "@mobilestock-native/tools";
+/* eslint-disable @typescript-eslint/no-require-imports */
 import { Platform } from "react-native";
 
 function anToWeSize(value: `${number}rem`) {
-  return Platform.OS === "android" ? tools.calculateSpacing(value) : value;
+  const t = require("@mobilestock-native/tools");
+  return Platform.OS === "android" ? t.default.calculateSpacing(value) : value;
 }
 function anToWeColumns(value: number) {
   return Platform.OS === "android" ? Math.ceil(value / 3) : value;
+}
+function anToWeFontSize(value: `${number}rem`) {
+  const Typo =
+    Platform.OS === "web"
+      ? require("@mobilestockweb/tools")
+      : require("@mobilestock-native/tools");
+  return Platform.OS === "android"
+    ? Typo.default.calculateFontSize(value)
+    : value;
 }
 
 export default {
@@ -42,6 +52,15 @@ export default {
       xl: anToWeSize("1.5rem"),
       "2xl": anToWeSize("1.75rem"),
       "3xl": anToWeSize("2rem"),
+    },
+    font: {
+      xs: anToWeFontSize("0.75rem"),
+      sm: anToWeFontSize("0.875rem"),
+      md: anToWeFontSize("1rem"),
+      lg: anToWeFontSize("1.25rem"),
+      xl: anToWeFontSize("1.75rem"),
+      "2xl": anToWeFontSize("1.875rem"),
+      "3xl": anToWeFontSize("2rem"),
     },
   },
 } as const;
