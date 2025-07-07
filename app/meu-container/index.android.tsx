42e9df722d223f6f4bb2245816629c4024f3a574
Gustavo210 <gustavogontijoo@gmail.com>
2025-07-07 14:54:46 -0300

    tela web e native

--- app/meu-container/index.android.tsx
+++ app/meu-container/index.android.tsx
@@ -1,13 +1,49 @@
+import { Container } from "@/COMPONENTE/Native";
 import { Typography } from "@mobilestock-native/typography";
 import React from "react";
-import { styled } from "styled-components/native";
 
 export default function IndexAndroid() {
   return (
-    <AndroidContainer>
-      <Typography>Rota Android para meu-container</Typography>
-    </AndroidContainer>
+    <Container.Y>
+      <Container.X>
+        <Typography align="CENTER">meu container 1</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 2</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 3</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 4</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 5</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 6</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 7</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 8</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 9</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 10</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 11</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 12</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 12</Typography>
+      </Container.X>
+    </Container.Y>
   );
 }
-
-const AndroidContainer = styled.View``;
--- app/meu-container/index.web.tsx
+++ app/meu-container/index.web.tsx
@@ -1,13 +1,49 @@
+import { Container } from "@/COMPONENTE/Web";
 import { Typography } from "@mobilestockweb/typography";
 import React from "react";
-import { styled } from "styled-components";
 
 export default function IndexWeb() {
   return (
-    <WebContainer>
-      <Typography>Rota Web para meu-container</Typography>
-    </WebContainer>
+    <Container.Y>
+      <Container.X>
+        <Typography align="CENTER">meu container 1</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 2</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 3</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 4</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 5</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 6</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 7</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 8</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 9</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 10</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 11</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 12</Typography>
+      </Container.X>
+      <Container.X>
+        <Typography align="CENTER">meu container 12</Typography>
+      </Container.X>
+    </Container.Y>
   );
 }
-
-const WebContainer = styled.div``;
